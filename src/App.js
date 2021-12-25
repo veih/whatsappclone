1f01b127282f767e6ae46183893a227c3e1030af
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2021-12-25 17:08:38 -0300

    novos arquivos.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.css';
+
+export default () => {
+  return (
+    <div className="app-window">
+        <div className="sidebar">
+          <header>
+            <img src="" alt=""/>
+          </header>
+          <div className="searhch">
+            ...
+          </div>
+          <div className="chatlist">
+            ...
+          </div>
+        </div>
+        <div className="contentarea">
+          ...
+        </div>
+    </div>
+  );
+}
